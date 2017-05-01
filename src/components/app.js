547bd7f799c3ee780903e887bb476e053d9b0b3e
Nick Wu <nickwu78@gmail.com>
2017-05-01 23:19:31 +1000

    SongList added

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import styles from './app.css';
 import SearchBar from './SearchBar/SearchBar';
 import searchSpotify from '../utils/searchSpotify';
 import SongItem from './SongItem/SongItem';
+import SongList from './SongList/SongList';
 
 export default class App extends Component {
 
@@ -11,6 +13,7 @@ export default class App extends Component {
       initialMessage: 'greeting',
       song: '',
       tracks: {},
+      songPosition: 0,
     };
   }
 
@@ -20,12 +23,13 @@ export default class App extends Component {
   }
 
   render() {
-    const { tracks } = this.state;
-    console.log('tracks', tracks);
+    const { tracks, songPosition } = this.state;
+    console.log('tracks', tracks.tracks);
     return (
-      <div>
+      <div className={styles.root}>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
-        {tracks.items && <SongItem songData={tracks.items[0]} />}
+        {tracks.tracks && <SongItem songData={tracks.tracks.items[songPosition]} />}
+        {tracks.tracks && <SongList listOfSongs={tracks.tracks.items} selectSong={(songPosition) => this.setState({ songPosition })} />}
       </div>
     );
   }
